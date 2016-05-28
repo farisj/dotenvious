f5121b6b8eb0b240110e32ef2b5286950e9e6778
farisj <jakefaris@outlook.com>
2016-05-28 12:04:10 -0400

    suppress terminal output during tests

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -4,5 +4,17 @@ Bundler.setup
 require 'dotenvious' # and any other gems you need
 
 RSpec.configure do |config|
-  # some (optional) config here
+  #copied & pasted from http://stackoverflow.com/questions/15430551
+  #whoops
+  original_stderr = $stderr
+  original_stdout = $stdout
+  config.before(:all) do
+    # Redirect stderr and stdout
+    $stderr = File.open(File::NULL, "w")
+    $stdout = File.open(File::NULL, "w")
+  end
+  config.after(:all) do
+    $stderr = original_stderr
+    $stdout = original_stdout
+  end
 end
