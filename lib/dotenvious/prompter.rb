5a49aa455b9bbb633f817b2758a306488c26ae75
farisj <jakefaris@outlook.com>
2016-06-04 13:37:43 -0400

    refactor Prompter to be a little DRY-er

--- lib/dotenvious/prompter.rb
+++ lib/dotenvious/prompter.rb
@@ -1,20 +1,19 @@
 require_relative 'value_replacer'
 require_relative 'mismatched_variable_finder'
+require_relative 'prompter'
 
 module Dotenvious
   class Prompter
     def self.run
       keys_in_question.each do |key, status|
-        case status
-        when 'missing'
-          decision = prompt_for_append(key)
-          return if decision.downcase == 'q'
-          env.write(env_entry(key) + "\n") if decision.downcase == 'y'
-        when 'mismatched'
-          # do mismatched stuff
-          decision = prompt_for_replace(key)
-          return if decision.downcase == 'q'
-          ValueReplacer.new.replace(key) if decision.downcase == 'y'
+        decision = prompt(key, status)
+        return if decision == 'q'
+        next unless decision.downcase == 'y'
+
+        if status == 'missing'
+          env.write(env_entry(key) + "\n")
+        elsif status == 'mismatched'
+          ValueReplacer.new.replace(key)
         end
       end
     end
@@ -35,17 +34,24 @@ module Dotenvious
       MismatchedVariableFinder.mismatched_vars
     end
 
-    def self.prompt_for_append(var)
-      puts env_entry(var)
-      puts "Add to .env? [y/n/q]"
+    def self.prompt(var, status)
+      if status == 'missing'
+        display_missing_output(var)
+      elsif status == 'mismatched'
+        display_mismatch_output(var)
+      end
       STDIN.gets.strip
     end
 
-    def self.prompt_for_replace(var)
+    def self.display_missing_output(var)
+      puts env_entry(var)
+      puts "Add to .env? [y/n/q]"
+    end
+
+    def self.display_mismatch_output(var)
       puts "ENV[#{var}] is set to: #{ENV[var]}"
       puts "Example [#{var}] is set to: #{ENV_EXAMPLE[var]}"
       puts "Replace with the example value? [y/n/q]"
-      STDIN.gets.strip
     end
 
     def self.env_entry(var)
