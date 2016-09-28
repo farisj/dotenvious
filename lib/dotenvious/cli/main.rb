572c951b0d6c58f50da844a679219b45b9ece337
farisj <jakefaris@outlook.com>
2016-09-27 21:01:44 -0400

    Prevent user from adding flags on command line

--- lib/dotenvious/cli/main.rb
+++ lib/dotenvious/cli/main.rb
@@ -12,12 +12,16 @@ module Dotenvious
       end
 
       def run
-        Loaders::Configuration.new.load
-        Loaders::Environments.new(filename).load_envs
-        unless all_vars_present? && all_vars_match?
-          alert_user
-          decision = STDIN.gets.strip
-          Prompter.run if decision.downcase == 'y'
+        if ARGV[0].to_s.empty?
+          Loaders::Configuration.new.load
+          Loaders::Environments.new(filename).load_envs
+          unless all_vars_present? && all_vars_match?
+            alert_user
+            decision = STDIN.gets.strip
+            Prompter.run if decision.downcase == 'y'
+          end
+        else
+          ask_user_to_remove_flags
         end
       end
 
@@ -27,6 +31,10 @@ module Dotenvious
 
       def alert_user
         puts "You have missing ENV variables. Examime them? [y/n]"
+      end
+
+      def ask_user_to_remove_flags
+        puts "dotenvious does not have flags at this time. Run 'dotenvious' without flags to get the main functionality."
       end
 
       def all_vars_present?
--- spec/dotenvious/cli/main_spec.rb
+++ spec/dotenvious/cli/main_spec.rb
@@ -8,42 +8,58 @@ describe Dotenvious::CLI::Main do
   end
 
   describe '#run' do
-    before do
-      allow(File).to receive(:read).and_return("")
-    end
-    it 'loads the environment & example environment variables' do
-      expect_any_instance_of(Dotenvious::Loaders::Environments).to receive(:load_envs)
+    context 'with no flags' do
+      before do
+        allow(File).to receive(:read).and_return("")
+      end
 
-      described_class.new.run
-    end
+      it 'loads the environment & example environment variables' do
+        expect_any_instance_of(Dotenvious::Loaders::Environments).to receive(:load_envs)
 
-    context 'when there are ENV vars missing' do
-      context 'and the user wants to append them' do
-        it 'begins the Prompter' do
-          io_object = double
-          expect(STDIN).to receive(:gets).and_return('y')
-          expect_any_instance_of(described_class).to receive(:all_vars_present?).and_return false
-          expect(Dotenvious::Prompter).to receive(:run)#.and_return false
-          described_class.new.run
+        described_class.new.run
+      end
+
+      context 'when there are ENV vars missing' do
+        context 'and the user wants to append them' do
+          it 'begins the Prompter' do
+            io_object = double
+            expect(STDIN).to receive(:gets).and_return('y')
+            expect_any_instance_of(described_class).to receive(:all_vars_present?).and_return false
+            expect(Dotenvious::Prompter).to receive(:run)#.and_return false
+            described_class.new.run
+          end
+        end
+
+        context 'but the user does not care' do
+          it 'quits' do
+            expect(STDIN).to receive(:gets).and_return('n')
+            expect_any_instance_of(described_class).to receive(:all_vars_present?).and_return false
+            expect(Dotenvious::Prompter).not_to receive(:run)#.and_return false
+
+            described_class.new.run
+          end
         end
       end
-      context 'but the user does not care' do
-        it 'quits' do
-          expect(STDIN).to receive(:gets).and_return('n')
-          expect_any_instance_of(described_class).to receive(:all_vars_present?).and_return false
-          expect(Dotenvious::Prompter).not_to receive(:run)#.and_return false
 
-          described_class.new.run
+      context 'when there are no ENV vars missing or mismatched' do
+        it 'does nothing' do
+          expect_any_instance_of(described_class).to receive(:all_vars_present?).and_return true
+          expect_any_instance_of(described_class).to receive(:all_vars_match?).and_return true
+
+          expect { described_class.new.run }.to_not raise_error
         end
       end
     end
 
-    context 'when there are no ENV vars missing or mismatched' do
-      it 'does nothing' do
-        expect_any_instance_of(described_class).to receive(:all_vars_present?).and_return true
-        expect_any_instance_of(described_class).to receive(:all_vars_match?).and_return true
+    context 'when option flags are given' do
+      before do
+        stub_const('ARGV', ['--test'])
+      end
 
-        expect { described_class.new.run }.to_not raise_error
+      it 'tells the user to try again without flags' do
+        expect_any_instance_of(described_class).to receive(:ask_user_to_remove_flags)
+
+        described_class.new.run
       end
     end
   end
