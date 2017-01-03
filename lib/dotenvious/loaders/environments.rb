7ea30ae5c31ab930db1d79a0a7d5ab5c4508a8fe
farisj <jakefaris@outlook.com>
2017-01-03 00:20:00 -0800

    Allow User to specify which example file to use

--- lib/dotenvious/loaders/environments.rb
+++ lib/dotenvious/loaders/environments.rb
@@ -3,10 +3,18 @@ require_relative 'dotenv_file'
 module Dotenvious
   module Loaders
     class Environments
+      def initialize(options = {})
+        @example_file = options[:example_file] || DEFAULT_EXAMPLE_ENV_FILE
+      end
+
       def load_environments
         ENV.merge!(DotenvFile.load_from('.env'))
-        ENV_EXAMPLE.merge!(DotenvFile.load_from('.example-env'))
+        ENV_EXAMPLE.merge!(DotenvFile.load_from(example_file))
       end
+
+      private
+
+      attr_reader :example_file
     end
   end
 end
