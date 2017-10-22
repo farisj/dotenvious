ffd81cc9a757552484b3dbfc990087042d271694
farisj <jakefaris@outlook.com>
2017-10-22 11:50:01 -0400

    [Bug] Add newline at end of final replaced value

--- spec/dotenvious/value_replacer_spec.rb
+++ spec/dotenvious/value_replacer_spec.rb
@@ -3,21 +3,24 @@ require 'spec_helper'
 describe Dotenvious::ValueReplacer do
   describe '#replace' do
     before do
-      stub_const('Dotenvious::ENV_EXAMPLE', {'fake' => 'correct'} )
+      stub_const('Dotenvious::ENV_EXAMPLE', example_const )
     end
-    it "replaces the key's value in .env if user presses yes" do
-      expect(File).to receive(:read).
-        with('.big-ol-env').
-        and_return("test=1234\nfake=missing")
+    context 'given a key with a different value' do
+      let(:example_const) { {'fake' => 'correct'} }
+      it "replaces the key's value in .env if user presses yes" do
+        expect(File).to receive(:read).
+          with('.big-ol-env').
+          and_return("test=1234\nfake=missing")
 
-      env_double = double('File', write: nil)
-      expect(env_double).to receive(:write).with("test=1234\nfake=correct")
+        env_double = double('File', write: nil)
+        expect(env_double).to receive(:write).with("test=1234\nfake=correct\n")
 
-      expect(File).to receive(:open).
-        with('.big-ol-env', 'w').
-        and_return(env_double)
+        expect(File).to receive(:open).
+          with('.big-ol-env', 'w').
+          and_return(env_double)
 
-      described_class.new('.big-ol-env').replace('fake')
+        described_class.new('.big-ol-env').replace('fake')
+      end
     end
   end
 end
